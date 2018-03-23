a02eaf8647b70393bfb2429930537c372af3622f
Daniel Madrigal <danieltristan1991@gmail.com>
2018-03-23 13:09:11 +0100

    Agregar archivos de proyecto.

---
+++ Fichero/Alumno.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fichero
+{
+    class Alumno
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public string Dni { get; set; }
+
+        public Alumno()
+        {
+            Console.WriteLine("Introduzca el Id:");
+            this.Id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Introduzca el Nombre:");
+            this.Nombre = Console.ReadLine();
+            Console.WriteLine("Introduzca el Apellido:");
+            this.Apellidos = Console.ReadLine();
+            Console.WriteLine("Introduzca el Dni:");
+            this.Dni = Console.ReadLine();
+
+        }
+    }
+}
---
+++ Fichero/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Fichero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream flujo = new FileStream("Registro.txt",FileMode.Create,FileAccess.Write);
+            StreamWriter fichero = new StreamWriter(flujo);
+          
+            
+            Alumno alumno;
+            int opcion=1;
+            do
+            {
+
+                Console.WriteLine("Que quiere hacer :");
+                Console.WriteLine("1.Registrar alumno" + "\n" + "0.Sair");
+                opcion = Convert.ToInt32(Console.ReadLine());
+                if (opcion==1)
+                {
+                    Console.WriteLine("Elije al formato :"+"\n"+"1.Texto"+"\n"+"2.Json"+"\n"+"3.Xml");
+                    int formato = Convert.ToInt32(Console.ReadLine());
+                    switch (formato)
+                    {
+                        case 1:
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        default:
+                            alumno = new Alumno();
+                            string registro = String.Format(alumno.Id + "," + alumno.Nombre + "," + alumno.Apellidos + "," + alumno.Dni.ToString());
+                            fichero.WriteLine(registro);
+                            break;
+                    }
+                   
+                }
+
+            } while (opcion!=0);
+
+            fichero.Close();
+            flujo.Close();
+        }
+    }
+}
